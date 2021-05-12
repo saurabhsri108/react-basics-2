38e308701ea85c1c4d75c374ad4a170cacaf51db
vasudeveloper001 <vasudeveloper001@gmail.com>
2021-05-12 21:04:49 +0530

    Updates

---
+++ precache-manifest.0934285b0c562a31294795870a285268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9b1471e3ed2822cd1d6",
+    "url": "/react-basics-2/static/css/main.a8ab740d.chunk.css"
+  },
+  {
+    "revision": "d9b1471e3ed2822cd1d6",
+    "url": "/react-basics-2/static/js/main.a5670b7b.chunk.js"
+  },
+  {
+    "revision": "576f7a204ccf4a9a298b",
+    "url": "/react-basics-2/static/js/runtime~main.5dd50ee9.js"
+  },
+  {
+    "revision": "f5bb35d3147ec8b006b8",
+    "url": "/react-basics-2/static/js/2.b0f1a594.chunk.js"
+  },
+  {
+    "revision": "044cb4c367811235dd9595cda47edfeb",
+    "url": "/react-basics-2/index.html"
+  }
+];
